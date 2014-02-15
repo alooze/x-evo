279a4595974e21b016784b6e023d829bb72f471b
alooze <a.looze@gmail.com>
2014-02-15 14:36:32 +0200

    Добавлен код сниппета

---
+++ assets/extensions/AutoFilter/snippets/autoFilter.snippet.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * autoFilter.snippet.inc.php
+ * 
+ * Является частью расширения autoFilter для MODX Evolution
+ * @version 1.0.0
+ * @author alooze (a.looze@gmail.com)
+ */
+$params = $modx->event->params;
+return include MODX_BASE_PATH.'assets/extensions/AutoFilter/snippets/autoFilter.snippet.inc.php';
+?>
